9e79c6d7bd00469fbffc441fb5752ddfdb71390c
Raghu Betina <raghu@firstdraft.com>
2018-03-11 07:29:35 +0000

    Only log user can access show page

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,5 +1,5 @@
 class LogsController < ApplicationController
-  before_action :current_user_must_be_log_user, :only => [:edit, :update, :destroy]
+  before_action :current_user_must_be_log_user, :only => [:show, :edit, :update, :destroy]
 
   def current_user_must_be_log_user
     log = Log.find(params[:id])
