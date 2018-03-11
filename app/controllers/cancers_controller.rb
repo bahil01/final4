1e5337297040294c5b59bcbd82e14cda4ebc0e45
Raghu Betina <raghu@firstdraft.com>
2018-03-11 07:29:35 +0000

    Only cancer user can access show page

--- app/controllers/cancers_controller.rb
+++ app/controllers/cancers_controller.rb
@@ -1,5 +1,5 @@
 class CancersController < ApplicationController
-  before_action :current_user_must_be_cancer_user, :only => [:edit, :update, :destroy]
+  before_action :current_user_must_be_cancer_user, :only => [:show, :edit, :update, :destroy]
 
   def current_user_must_be_cancer_user
     cancer = Cancer.find(params[:id])
