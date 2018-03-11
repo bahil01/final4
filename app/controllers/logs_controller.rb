4aa531bcf2253a3bf1fdf75c44c6c912d199b8ac
Raghu Betina <raghu@firstdraft.com>
2018-03-11 07:29:35 +0000

    Only log user can modify log

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,4 +1,14 @@
 class LogsController < ApplicationController
+  before_action :current_user_must_be_log_user, :only => [:edit, :update, :destroy]
+
+  def current_user_must_be_log_user
+    log = Log.find(params[:id])
+
+    unless current_user == log.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @logs = Log.all
 
